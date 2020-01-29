32d171fafe9592da81e90500f721afb0d0b38a62
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-01-29 20:55:34 +0200

    added task servlet

--- src/main/java/org/fasttrackit/persistance/TaskRepository.java
+++ src/main/java/org/fasttrackit/persistance/TaskRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class TaskRepository {
 
-    public void createTask(CreateTaskRequest request) throws IOException, SQLException {
+    public void createTask(CreateTaskRequest request) throws IOException, SQLException, ClassNotFoundException {
         // preventing SQL Injection by avoiding concatenation and using PreparedStatement
         String sql = "INSERT INTO task (description, deadline) VALUES (?,?)";
 
@@ -25,7 +25,7 @@ public class TaskRepository {
         }
     }
 
-    public void updateTask(long id, UpdateTaskRequest request) throws IOException, SQLException {
+    public void updateTask(long id, UpdateTaskRequest request) throws IOException, SQLException, ClassNotFoundException {
         // preventing SQL Injection by avoiding concatenation and using PreparedStatement
         String sql = "UPDATE task SET done = ? WHERE id = ?";
 
@@ -39,7 +39,7 @@ public class TaskRepository {
         }
     }
 
-    public void deleteTask(long id) throws IOException, SQLException {
+    public void deleteTask(long id) throws IOException, SQLException, ClassNotFoundException {
         // preventing SQL Injection by avoiding concatenation and using PreparedStatement
         String sql = "DELETE FROM task WHERE id = ?";
 
@@ -52,7 +52,7 @@ public class TaskRepository {
         }
     }
 
-    public List<Task> getTasks() throws IOException, SQLException {
+    public List<Task> getTasks() throws IOException, SQLException, ClassNotFoundException {
         String sql = "SELECT id, description, deadline, done FROM task";
 
         try (Connection connection = DatabaseConfiguration.getConnection();
